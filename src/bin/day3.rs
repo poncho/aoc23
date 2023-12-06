54373f744feeb13c81b2e33726d8e90b14a051dc
poncho <poncho.811@gmail.com>
2023-12-05 20:35:11 -0600

    Day 3: Part 2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -19,8 +19,10 @@ fn main() {
     let input = read_input("inputs/day3.txt");
 
     let p1_result = puzzle1(&input);
+    let p2_result = puzzle2(&input);
 
     println!("Puzzle #1: {}", p1_result);
+    println!("Puzzle #2: {}", p2_result);
 }
 
 fn read_input(path: &str) -> Day3 {
@@ -163,14 +165,90 @@ fn check_if_adjacent(
     false
 }
 
+fn puzzle2(input: &Day3) -> u32 {
+    let mut gears: Vec<u32> = vec![];
+
+    for row in input.iter() {
+        for part in row.into_iter().filter(|p| match p.part_type {
+            EnginePartType::Number(_) => false,
+            EnginePartType::Part('*') => true,
+            EnginePartType::Part(_) => false,
+        }) {
+            if let Some(gear) = get_gear_ratio(&input, &part) {
+                gears.push(gear)
+            }
+        }
+    }
+
+    gears.iter().sum()
+}
+
+fn get_gear_ratio(
+    input: &Day3,
+    &EnginePart {
+        part_type: _,
+        row,
+        start_index,
+    }: &EnginePart,
+) -> Option<u32> {
+    let mut numbers: Vec<String> = vec![];
+
+    let min = if start_index == 0 { 0 } else { start_index - 1 };
+    let max = start_index + 1;
+
+    // LEFT RIGHT
+    add_valid_numbers(&input[row], &mut numbers, min, max);
+
+    // TOP
+    if row > 0 {
+        add_valid_numbers(&input[row - 1], &mut numbers, min, max);
+    }
+
+    // BOTTOM
+    if row < input.len() - 1 {
+        add_valid_numbers(&input[row + 1], &mut numbers, min, max);
+    }
+
+    if numbers.len() == 2 {
+        let ratio: u32 = numbers
+            .iter()
+            .fold(1, |acc, n| acc * n.parse::<u32>().unwrap());
+
+        return Some(ratio);
+    }
+
+    None
+}
+
+fn add_valid_numbers(row: &Vec<EnginePart>, numbers: &mut Vec<String>, min: usize, max: usize) {
+    row.iter().for_each(|p| match &p.part_type {
+        EnginePartType::Part(_) => (),
+        EnginePartType::Number(number) => {
+            let end_index = &p.start_index + number.len() - 1;
+            if (min..=max).contains(&p.start_index) || (min..=max).contains(&end_index) {
+                numbers.push(number.to_string());
+            }
+        }
+    });
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const TEST_FILE: &str = "inputs/day3_test.txt";
+
     #[test]
     fn puzzle1_test() {
-        let test_input = read_input("inputs/day3_test.txt");
+        let test_input = read_input(TEST_FILE);
 
         assert_eq!(puzzle1(&test_input), 4361)
+    }
+
+    #[test]
+    fn puzzle2_test() {
+        let test_input = read_input(TEST_FILE);
+
+        assert_eq!(puzzle2(&test_input), 467835);
     }
 }
